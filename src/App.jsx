4c153dc725b585c827faaee0df0771b0bb9d8c3b
Picsaoclone <122031438+Picsaoclone@users.noreply.github.com>
2025-09-24 02:04:13 +0700

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World from ReactTTTT!</h1>
+      <h1>Hello World from Supa React!</h1>
       <div className="card">
         {/* Link thay vì button */}
         <a 
