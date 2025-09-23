24268da3d29d2908a6d8978d01097811497727fd
Picsaoclone <122031438+Picsaoclone@users.noreply.github.com>
2025-09-24 01:58:24 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World from ReactTTTT!</h1>
+      <div className="card">
+        {/* Link thay vì button */}
+        <a 
+          href="https://www.facebook.com/nguyen.huy.hoang.277341/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="link-button"
+        >
+         My info
+        </a>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
