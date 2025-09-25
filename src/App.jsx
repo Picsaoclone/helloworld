7aca98f0e56602d6fe6a3d84a54ce10c271649db
Picsaoclone <122031438+Picsaoclone@users.noreply.github.com>
2025-09-25 08:24:20 +0700

    new changes today

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World from Supa Reacttt!</h1>
+      <h1>Hello World from Supa Reactttt!</h1>
       <div className="card">
         {/* Link thay vì button */}
         <a 
